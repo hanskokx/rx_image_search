db549465ec41476b8a7e382b7325b2f80181538d
Hans Kokx <hans.d.kokx@gmail.com>
2021-11-29 14:07:39 -0500

    Cap image title at 2 lines and truncate with ellipsis
    Signed-off-by: Hans Kokx <hans.d.kokx@gmail.com>

--- lib/screens/image_detail_screen.dart
+++ lib/screens/image_detail_screen.dart
@@ -21,54 +21,67 @@ class _ImageDetailScreenState extends State<ImageDetailScreen> {
     final args = ModalRoute.of(context)!.settings.arguments as ScreenArguments;
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Text(
-              args.imageResult.title,
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            Container(
-              clipBehavior: Clip.antiAlias,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                image: const DecorationImage(
-                  repeat: ImageRepeat.repeat,
-                  image: AssetImage('assets/images/checkerboard.png'),
-                ),
-              ),
-              child: CachedNetworkImage(
-                imageUrl: args.imageResult.original,
-                errorWidget: (context, url, error) => Icon(
-                  Icons.cloud_off_outlined,
-                  color: Theme.of(context).errorColor,
-                  size: 64.0,
-                ),
-                progressIndicatorBuilder: (
-                  BuildContext context,
-                  String url,
-                  DownloadProgress progress,
-                ) {
-                  return Center(
-                    child: SizedBox(
-                      height: 100,
-                      width: 100,
-                      child: RiveAnimation.asset(
-                        'assets/animations/circular_progress.riv',
-                        animations: const [
-                          'infinite',
-                          'BackAndForth',
-                          '0to100'
-                        ],
-                        controllers: [_controller],
-                      ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Wrap(
+                  alignment: WrapAlignment.center,
+                  children: [
+                    Text(
+                      args.imageResult.title,
+                      textAlign: TextAlign.center,
+                      maxLines: 2,
+                      style: Theme.of(context).textTheme.headline6!.copyWith(
+                            overflow: TextOverflow.ellipsis,
+                          ),
                     ),
-                  );
-                },
+                  ],
+                ),
               ),
-            ),
-            DownloadImageButton(imageResult: args.imageResult),
-          ],
+              Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    repeat: ImageRepeat.repeat,
+                    image: AssetImage('assets/images/checkerboard.png'),
+                  ),
+                ),
+                child: CachedNetworkImage(
+                  imageUrl: args.imageResult.original,
+                  errorWidget: (context, url, error) => Icon(
+                    Icons.cloud_off_outlined,
+                    color: Theme.of(context).errorColor,
+                    size: 64.0,
+                  ),
+                  progressIndicatorBuilder: (
+                    BuildContext context,
+                    String url,
+                    DownloadProgress progress,
+                  ) {
+                    return Center(
+                      child: SizedBox(
+                        height: 100,
+                        width: 100,
+                        child: RiveAnimation.asset(
+                          'assets/animations/circular_progress.riv',
+                          animations: const [
+                            'infinite',
+                            'BackAndForth',
+                            '0to100'
+                          ],
+                          controllers: [_controller],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              DownloadImageButton(imageResult: args.imageResult),
+            ],
+          ),
         ),
       ),
     );
